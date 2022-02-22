45b39acb8519e41ddeae832fb8c049125187bd29
SkyNetCloud <mcboxer1201@gmail.com>
2022-02-22 10:46:37 -0500

    New Mod

---
+++ src/main/java/ca/skynetcloud/alloutpower/Alloutpower.java
@@ -0,0 +1,31 @@
+package ca.skynetcloud.alloutpower;
+
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("alloutpower")
+public class Alloutpower {
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Alloutpower() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+
+}
---
+++ src/main/java/ca/skynetcloud/alloutpower/client/event/SoulKeeperEvent.java
@@ -0,0 +1,4 @@
+package ca.skynetcloud.alloutpower.client.event;
+
+public class SoulKeeperEvent {
+}
